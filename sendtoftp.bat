eeb210a35b90b6dde4f3b8c06f07726dcd42707c
vladmalik <vladmalik@gmail.com>
2014-01-11 19:29:40 -0500

    Update sendtoftp.bat

--- sendtoftp.bat
+++ sendtoftp.bat
@@ -8,5 +8,5 @@ echo put %1>> ftpcmd.dat
 echo quit>> ftpcmd.dat
 ftp -n -s:ftpcmd.dat YOUR_FTP_SERVER
 del ftpcmd.dat
-"C:\Program Files (x86)\Mozilla Firefox\firefox" http://yoursite.com/uploads/
+"C:\Program Files (x86)\Mozilla Firefox\firefox" http://yoursite.com/uploads/%~n1%~x1
 exit
