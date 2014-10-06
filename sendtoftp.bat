1f5bde0cac8235a026d300cb57010c4e76bce1b2
vladmalik <vladmalik@gmail.com>
2014-10-05 23:05:55 -0400

    Update sendtoftp.bat

--- sendtoftp.bat
+++ sendtoftp.bat
@@ -1,12 +1,12 @@
 @echo off
-echo user YOUR_USERNAME> ftpcmd.dat
-echo YOUR_PASSWORD>> ftpcmd.dat
+echo user your_username> ftpcmd.dat
+echo your_password>> ftpcmd.dat
 echo cd your_public_html_folder>> ftpcmd.dat
 echo cd your_pdfs_folder>> ftpcmd.dat
 echo bin>> ftpcmd.dat
 echo put %1>> ftpcmd.dat
 echo quit>> ftpcmd.dat
-ftp -n -s:ftpcmd.dat YOUR_FTP_SERVER
+ftp -n -s:ftpcmd.dat ftp.yourserver.com
 del ftpcmd.dat
-"C:\Program Files (x86)\Mozilla Firefox\firefox" "http://yoursite.com/uploads/%~n1%~x1"
+"C:\Program Files (x86)\Mozilla Firefox\firefox" "http://yourserver.com/your_pfds_folder/%~n1%~x1"
 exit
